fecc471d0a4bca0e9e6cde0713a5eaec661a9a16
adamvangrover <adamvangrover@gmail.com>
2025-11-23 20:41:27 -0500

    Create config.js

---
+++ js/games/aetheria/config.js
@@ -0,0 +1,9 @@
+export const config = {
+    worldSize: 400,
+    chunkSize: 64, // Vertex density
+    waterLevel: -5,
+    mountainHeight: 35,
+    gravity: 0.8,
+    speed: 0.5,
+    jumpForce: 1.2
+};
